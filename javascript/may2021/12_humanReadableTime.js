3e1eced4777b87e968f315a77934ee50ec212db3
Megan Macdonald <mmacdonald@techtonic.com>
2021-05-13 09:52:47 -0600

    add human readable time challenge

---
+++ javascript/may2021/12_humanReadableTime.js
@@ -0,0 +1,15 @@
+// Challenge link: https://www.codewars.com/kata/52685f7382004e774f0001f7/train/javascript
+
+// seconds != epoch seconds
+// 0 <= seconds <= 359999 (99:59:59)
+const humanReadable = (seconds) => {
+  const hours = padZero(Math.floor(seconds / 3600));
+  const minutes = padZero(Math.floor((seconds % 3600) / 60));
+  const sec = padZero((seconds % 3600) % 60);
+
+  return `${hours}:${minutes}:${sec}`;
+};
+
+const padZero = (num) => {
+  return ("0" + num).slice(-2);
+};
