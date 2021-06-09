f18fcb80f9325dfc68a60d971248719f8f7e3919
Megan Macdonald <mmacdonald@techtonic.com>
2021-06-09 10:07:15 -0600

    quick dart challenges

---
+++ dart/29_easyChallenges.dart
@@ -0,0 +1,14 @@
+// Challenge Link: https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+String evenOrOdd(int number) {
+  return (number.isOdd) ? "Odd" : "Even";
+}
+
+// Challenge link: https://www.codewars.com/kata/56dec885c54a926dcd001095
+num opposite(num n) {
+  return 0 - n;
+}
+
+// Challenge link: https://www.codewars.com/kata/57a0e5c372292dd76d000d7e
+String repeatString(int n, String s) {
+  return s * n;
+}
